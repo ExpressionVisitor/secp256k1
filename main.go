ca2237d9f4d4e747f5a2b709d655fe8bbcb18030
Gitea <gitea@fake.local>
2018-10-22 17:36:15 +0800

    update comment

--- main.go
+++ main.go
@@ -8,12 +8,9 @@ import (
 func main() {
 
 	/*================Sign begin================*/
-	// data 待签名的数据
-	// 转账地址：phenix2CkSyo9K5rrdw7aV4gkEAYNfAFjAi7pEgH
-	// 接收地址：phenix2G9nhGgH8J5w8E2cF18DGiqxUq9Lf7p468
-	// 转账金额：1.6
+	//转出地址|转入地址|转账金额
 	data := []byte("phenix2CkSyo9K5rrdw7aV4gkEAYNfAFjAi7pEgH|phenix2G9nhGgH8J5w8E2cF18DGiqxUq9Lf7p468|1.6")
-	//私钥
+	//转出地址对应的私钥
 	priKey := []byte("lVKXDndWw1yJBuJXYNUxm0IA31dmOVQX")
 	//签名
 	sign, err := Sign(priKey, data)
@@ -23,7 +20,7 @@ func main() {
 	/*================Sign end================*/
 
 	/*================Verify begin================*/
-	//公钥
+	//转出地址对应的公钥
 	pubKey := "04884fa0ce7d1310ab87fbd2680a21959db648ff6771248f5e2fecc45179fdbd26039b5684f6cdf5fb4f2f288e12cb982a1b3fc84b112f3cbba1b4e47ac1e04a73"
 	//验证签名
 	flag, err := Verify(sign, data, pubKey)
